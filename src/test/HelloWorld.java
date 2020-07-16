e83f6194ad660d15d292e5ab84a858536defb331
Nilesh Gavhane <Nilesh.Gavhane@veritas.com>
2020-07-16 19:46:55 +0530

    first Hello world program

---
+++ src/test/HelloWorld.java
@@ -0,0 +1,10 @@
+package test;
+
+public class HelloWorld {
+	
+	//this function print normal "hello world" text
+	public static void main(String args[]) {
+		System.out.println("Welcome to java..!!");
+	}
+
+}// main class complete
