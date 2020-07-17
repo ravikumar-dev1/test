143a87fdbdada79eb3b96c41f00ef5166366eaad
Nilesh Gavhane <Nilesh.Gavhane@veritas.com>
2020-07-17 19:46:42 +0530

    data types introduction

--- src/test/HelloWorld.java
+++ src/test/HelloWorld.java
@@ -2,9 +2,24 @@ package test;
 
 public class HelloWorld {
 	
+	static String studentName = "ABC";//global variable
+	
 	//this function print normal "hello world" text
 	public static void main(String args[]) {
-		System.out.println("Welcome to java..!!");
+		//store student information
+		String student_name = "Rakesh";//local variable
+		
+		float percentage = 95.46F;
+		int subCode = 02;
+		
+		char gender = 'M';
+		//System.out.println("student Name: " + studentName);
+		//System.out.println("Student Gender " + gender);
+		boolean isPass = true;
+		
+		System.out.println("student Name: " + studentName);
 	}
+	
+	
 
 }// main class complete
